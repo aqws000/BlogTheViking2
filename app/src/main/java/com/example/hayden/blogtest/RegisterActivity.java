454353a901300eaddcd8b2b692d96e155595c022
aqws000 <captaincushing@aol.com>
2018-08-30 18:27:00 -0400

    Finished Register Activity,
    You can now register an account on the App

--- app/src/main/java/com/example/hayden/blogtest/RegisterActivity.java
+++ app/src/main/java/com/example/hayden/blogtest/RegisterActivity.java
@@ -1,19 +1,23 @@
 package com.example.hayden.blogtest;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 private EditText mNameField;
@@ -24,53 +28,88 @@ private EditText mNameField;
 
     private FirebaseAuth mAuth;
 
-    private ProgressDialog mProgress;
+    private DatabaseReference mDatabase;
 
+    private ProgressDialog mProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+
         mAuth = FirebaseAuth.getInstance();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+        //Creating new Users child
+
+
 
         mProgress = new ProgressDialog(this);
 
         mNameField = (EditText) findViewById(R.id.nameField);
-        mEmailField =(EditText) findViewById(R.id.emailField);
+
+        mEmailField = (EditText) findViewById(R.id.emailField);
+
         mPasswordField = (EditText) findViewById(R.id.passwordField);
-        mRegisterBtn = (Button) findViewById(R.id.registrationBtn);
+
+        mRegisterBtn = (Button) findViewById(R.id.registerBtn);
+        //All related to Activity_register xml
+
 
         mRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 startRegister();
+                //Button function
+
             }
         });
+
 
     }
 
     private void startRegister() {
 
-        String name =mNameField.getText().toString().trim();
-        String email =mEmailField.getText().toString().trim();
-        String password =mPasswordField.getText().toString().trim();
+        final String name = mNameField.getText().toString().trim();
+        String email = mEmailField.getText().toString().trim();
+        String password = mPasswordField.getText().toString().trim();
 
-        if(TextUtils.isEmpty(name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
-            //All of these have to not be empty in order to proceed with registration
+        if(!TextUtils.isEmpty(name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
+//If any of the fields are empty, the sign up will not work
+            mProgress.setMessage("Signing Up ...");
+            mProgress.show();
 
-            mProgress.
             mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
+
                     if(task.isSuccessful()){
 
-                    }
-                }
-            })
+                        String user_id = mAuth.getCurrentUser().getUid();
 
+                        DatabaseReference current_user =  mDatabase.child(user_id);
+
+                        current_user.child("name").setValue(name);
+                        current_user.child("image").setValue("default");
+                        //Creating childs name and image under Users child
+
+                        mProgress.dismiss();
+
+                        Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                        mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(mainIntent);
+                        //Then logs you onto the main page
+
+                    }
+
+                }
+            });
 
         }
 
     }
 }
+
+
